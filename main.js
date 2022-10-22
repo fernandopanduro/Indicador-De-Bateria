84515b148efbe5c2715f9918b6444ba9a9fdec17
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-10-22 17:04:18 -0600

    Agregado estilos a la bateria

--- main.js
+++ main.js
@@ -1,7 +1,57 @@
-let percentage = document.querySelector('.percentage');
-let percent = document.querySelector('.percent');
+/* Battery */
 
-navigator.getBattery().then(function (battery) {
-    percentage.style.width = battery.level * 100 + '%';
-    percent.innerHTML = battery.level * 100 + '%';
-})
+initBattery();
+
+function initBattery() {
+    let percentage = document.querySelector('.percentage');
+    let percent = document.querySelector('.percent');   
+    let status = document.querySelector('.status');   
+
+    navigator.getBattery().then(function (battery) {
+
+        updateBattery = () => {
+            let level = Math.floor(battery.level * 100);
+            percent.innerHTML = level + '%';
+            percentage.style.width = level + '%';
+
+            if (level == 100) {
+                status.innerHTML = 'Full Battery'
+            }
+            else if (level <= 20 &! battery.charging) {
+                status.innerHTML = 'Low Battery'
+            }
+            else if (battery.charging) {
+                status.innerHTML = 'Charging...'
+            }
+            else {
+                status.innerHTML - ''
+            }
+
+
+            if (level <= 20) {
+                percentage.classList.add('gradient-color-red');
+                percentage.classList.remove('gradient-color-green', 'gradient-color-orange', 'gradient-color-yellow')
+            } 
+            else if (level <= 40) {
+                percentage.classList.add('gradient-color-orange');
+                percentage.classList.remove('gradient-color-green', 'gradient-color-red', 'gradient-color-yellow')                
+            }
+            else if (level <= 80) {
+                percentage.classList.add('gradient-color-yellow');
+                percentage.classList.remove('gradient-color-green', 'gradient-color-orange', 'gradient-color-red')
+            } else {
+                percentage.classList.add('gradient-color-green');
+                percentage.classList.remove('gradient-color-red', 'gradient-color-orange', 'gradient-color-yellow')
+            }
+
+
+        };
+        updateBattery();
+
+        battery.addEventListener('chargingchange', () => {updateBattery()});
+        battery.addEventListener('levelchange', () => {updateBattery()});
+
+    })
+
+}
+
